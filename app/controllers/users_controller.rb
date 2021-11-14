b90c51948608730ef442761c19d6fd957cacd699
yuri-misonoo <yuri.panpanda@gmail.com>
2021-11-14 12:38:14 +0000

    [add]7.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       
     else
@@ -15,4 +15,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
