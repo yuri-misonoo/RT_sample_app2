9efa995a11282387dab6d8f66e49fad86edf265b
yuri-misonoo <yuri.panpanda@gmail.com>
2021-11-14 12:34:10 +0000

    [add]7.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      
+    else
+      render 'new'
+    end
+  end
 
   def show
     @user = User.find(params[:id])
